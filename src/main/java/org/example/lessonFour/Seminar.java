ae2e1e32ef112cf44c7aa47981f9c98af709ecee
Anton <B_atonyo_S@bk.ru>
2024-05-09 07:53:08 +0300

    pom.xml updating

---
+++ src/main/java/org/example/lessonFour/Seminar.java
@@ -0,0 +1,13 @@
+package org.example.lessonFour;
+
+/*Выполнять домашнее задание с семниара!!!
+
+Перенести структуру дз третьего урока на JPA:
+1. Описать сущности Student и Group
+2. Написать запросы: Find, Persist, Remove
+3. ... поупражняться с разными запросами ...*/
+public class Seminar {
+    public static void main(String[] args) {
+
+    }
+}
